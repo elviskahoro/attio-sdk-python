13b6a79bdd8d4a86a68b21b7f98d59737c33cbb8
elvis kahoro <github@elvis.ai>
2025-10-01 18:05:42 -0700

    done

--- src/attio/models/input_value_union.py
+++ src/attio/models/input_value_union.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 from attio.types import BaseModel, Nullable, OptionalNullable, UNSET, UNSET_SENTINEL
-from datetime import datetime
+from datetime import date, datetime
 import pydantic
 from pydantic import model_serializer
 from typing import List, Literal, Optional, Union
@@ -10,13 +10,13 @@ from typing_extensions import Annotated, NotRequired, TypeAliasType, TypedDict
 
 
 class InputValue18TypedDict(TypedDict):
-    value: str
-    r"""A timestamp value represents a single, universal moment in time using an ISO 8601 formatted string. This means that a timestamp consists of a date, a time (with nanosecond precision), and a time zone. Attio will coerce timestamps which do not provide full nanosecond precision and UTC is assumed if no time zone is provided. For example, \"2023\", \"2023-01\", \"2023-01-02\", \"2023-01-02T13:00\", \"2023-01-02T13:00:00\", and \"2023-01-02T13:00:00.000000000\" will all be coerced to \"2023-01-02T13:00:00.000000000Z\". Timestamps are always returned in UTC. For example, writing a timestamp value using the string \"2023-01-02T13:00:00.000000000+02:00\" will result in the value \"2023-01-02T11:00:00.000000000Z\" being returned. The maximum date is \"9999-12-31T23:59:59.999999999Z\"."""
+    value: date
+    r"""A timestamp value represents a single, universal moment in time using an ISO 8601 formatted string"""
 
 
 class InputValue18(BaseModel):
-    value: str
-    r"""A timestamp value represents a single, universal moment in time using an ISO 8601 formatted string. This means that a timestamp consists of a date, a time (with nanosecond precision), and a time zone. Attio will coerce timestamps which do not provide full nanosecond precision and UTC is assumed if no time zone is provided. For example, \"2023\", \"2023-01\", \"2023-01-02\", \"2023-01-02T13:00\", \"2023-01-02T13:00:00\", and \"2023-01-02T13:00:00.000000000\" will all be coerced to \"2023-01-02T13:00:00.000000000Z\". Timestamps are always returned in UTC. For example, writing a timestamp value using the string \"2023-01-02T13:00:00.000000000+02:00\" will result in the value \"2023-01-02T11:00:00.000000000Z\" being returned. The maximum date is \"9999-12-31T23:59:59.999999999Z\"."""
+    value: date
+    r"""A timestamp value represents a single, universal moment in time using an ISO 8601 formatted string"""
 
 
 class InputValue17TypedDict(TypedDict):
